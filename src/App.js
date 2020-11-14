cb5dbf47dd8ba06dab0eedfcc8f6026940330dac
Jung95 <jung95219@gmail.com>
2020-11-14 12:15:09 +0900

    Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from "react";
 import Potato from './Potato';
 
-function Food({fav, test}) {
+function Food({fav, picture}) {
   return (
-  <h3>I love {fav}{test}</h3>
+<div>
+  <h3>I love {fav}</h3>
+  <img src={picture} />
+  </div>
   );
 }
 
+const foodILike = [
+  {
+    name: "Kimchi",
+    image: "https://health.chosun.com/site/data/img_dir/2020/07/21/2020072103114_0.jpg"
+  }, 
+  {
+    name: "ramen",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Korea_Ramyeon.jpg/1920px-Korea_Ramyeon.jpg"
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <h1>Hellow</h1>
-        <Potato />
-        <Food fav="kimchi" test = "12" />
-        <Food fav="ramen" test = "23"  />
+      <h1>Hello </h1>
+        {foodILike.map(dish => <Food fav ={dish.name} picture={dish.image}/>)}
     </div>
   );
 }
